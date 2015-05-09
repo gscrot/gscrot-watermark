7a2e08f7bfa6ffe1072697d14f22dc46097d70e8
redpois0n <red@cmail.nu>
2015-05-09 20:39:17 +0200

    register processor

--- src/gscrot/processor/watermark/WatermarkPlugin.java
+++ src/gscrot/processor/watermark/WatermarkPlugin.java
@@ -1,11 +1,14 @@
 package gscrot.processor.watermark;
 
+import com.redpois0n.gscrot.ImageProcessor;
+
 import gscrot.api.Plugin;
 
 public class WatermarkPlugin extends Plugin {
 
 	public WatermarkPlugin() {
 		super("Watermark");
+		ImageProcessor.addProcessor(new WatermarkProcessor());
 	}
 
 }
