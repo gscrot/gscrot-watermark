cc5bd4d28c7edbdb0d7e8c2efe2aa8f9f0eeda00
redpois0n <red@cmail.nu>
2015-05-15 13:56:55 +0200

    String drawing

--- src/gscrot/processor/watermark/WatermarkProcessor.java
+++ src/gscrot/processor/watermark/WatermarkProcessor.java
@@ -1,5 +1,7 @@
 package gscrot.processor.watermark;
 
+import gscrot.processor.watermark.WatermarkPlugin.Mode;
+import gscrot.processor.watermark.WatermarkPlugin.Position;
 import iconlib.IconUtils;
 
 import java.awt.Graphics2D;
@@ -14,7 +16,24 @@ public class WatermarkProcessor extends GraphicsImageProcessor {
 
 	@Override
 	public void process(Graphics2D g, int width, int height) {
-		
+		if (WatermarkPlugin.mode == Mode.TEXT) {
+			g.setFont(WatermarkPlugin.font);
+			g.setColor(WatermarkPlugin.foreground);
+			
+			String s = WatermarkPlugin.string;
+			
+			if (WatermarkPlugin.position == Position.TOPLEFT) {
+				g.drawString(s, 10, 10);
+			} else if (WatermarkPlugin.position == Position.TOPRIGHT) {
+				g.drawString(s, width - g.getFontMetrics().stringWidth(s) - 10, 10);
+			} else if (WatermarkPlugin.position == Position.BOTTOMLEFT) {
+				g.drawString(s, 10, height - g.getFontMetrics().getHeight() - 10);
+			} else if (WatermarkPlugin.position == Position.BOTTOMRIGHT) {
+				g.drawString(s, width - g.getFontMetrics().stringWidth(s) - 10, height - g.getFontMetrics().getHeight() - 10);
+			}
+		} else if (WatermarkPlugin.mode == Mode.IMAGE) {
+			
+		}
 	}
 
 }
